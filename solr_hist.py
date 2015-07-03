8bb826c34cc395fad58f30316298dd730b9ed5a8
morgantrove <morgan@wapolabs.com>
2015-07-02 18:06:14 -0700

    solr-hist.py is more or less complete now

--- solr_hist.py
+++ solr_hist.py
@@ -68,68 +68,32 @@ def main(args):
     arg = args[0] # Takes a well-formed, whitespace-free solr query.
     r_str = ''
 
-    # ** Users must ensure they use the _ascii_ ' \" ', ie backslash & ascii-ambi-quote, in their arguments
-    #  Otherwise, the quotes are not recognized by SOLR.
+    # *  Users must ensure they use the _ASCII_ ' \" ', ie backslash & ascii-ambi-quote, in their arguments
+    # *  Otherwise, the quotes are not recognized by SOLR.  Example correct expression:
+    # *    \"Street+Food\"+OR+\"Food+Trucks\"+OR+\"Artisan+Food+Wagons\"+OR+\"Food+Wagons\"
 
     r_str = "http://solr.dev.trove.com:7979/solr/select?indent=on&version=2.2&q=%28" + arg + \
-            "%29&start=0&rows=2940&fl=facets&fq=&hl.fl=&explainOther=&qt=&wt=" 
+            "%29&start=0&rows=1&fl=facets&fq=&hl.fl=&explainOther=&qt=&wt=" 
 
     
-    print "requesting \'%s\'..." % r_str
+    print " > Requesting \'%s\'..." % r_str
     r = requests.get(r_str)
     if not r.status_code == 200:
         print "status: ", r.status_code, " <", r.reason, ">. Exiting."; return
-    else:
-        print "successful request."
-
-    ###  Channel format:  [string ID number, int count, string english label(, float normed count)]  ###
-
+    print " > Successful request.  Working..."
     f_chnls = getFacetChannels(r.text)
     sumCounts = 0.0 # <- float!
     for ch in f_chnls:
         sumCounts += ch[1]
     for ch in f_chnls:
         ch.append(ch[1] / sumCounts)
-        # disp_ch(ch[0], ch[1], ch[2], ch[3]) # if desired.
+        disp_ch(ch[0], ch[1], ch[2], ch[3]) # if desired.
 
 
-"""
-#####  find and rank the related channels:
-    text_str = r.text
-    contbit = True
-    startbit = True
-    chnls = []
-    while contbit:
-        rc_id = text_str.find(tag_name)
-        if rc_id == -1 or text_str.find('facet_counts') < rc_id: # <- end before facets section
-            contbit = False; break;
-        text_str = text_str[rc_id+1:]
-        if startbit:
-            startbit = False
-        else:
-            related = text_str[len(tag_name)+2:match_parenthesis(text_str)] # somewhat robust
-            related = re.sub('\"', '', related)
-            rels = related.split(",")
-            for r_ch in rels:
-                while not r_ch[0].isdigit():
-                    r_ch = r_ch[1:]
-                chnls.append(r_ch)
-    counts = []
-    for ch in chnls:
-        newl = findsubelem(ch, counts, 0)
-        if None == newl:
-            counts.append([ch, 1])
-        else:
-            newl[1] = newl[1]+1;
+    print " > Processing complete. "
 
-    counts.sort(key=lambda x: x[1])
-    counts.reverse()
-
-    print "All channels directly related to channel \'%s\':" % arg
-    for rc in counts:
-        print " -> ", rc[0], "(count:", rc[1], ")"
-
-"""
+    ###  Channel format:  [string ID number, int count, string english label, float normed count]  ###
+    ###  The formatted list of channels is the useful object to be returned or whatnot.
 
         
 
